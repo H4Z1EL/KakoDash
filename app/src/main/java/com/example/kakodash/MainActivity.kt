be297b3153d60ba316d2429260cb1febec2bd09b
Mitzi Yessenia Arrieta Ignacio <20243ds176@utez.edu.mx>
2025-12-11 00:20:16 -0600

    ruta "items" y GameScreen como pantalla inicial.​

--- app/src/main/java/com/example/kakodash/MainActivity.kt
+++ app/src/main/java/com/example/kakodash/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.kakodash
-//.
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.kakodash.ui.screens.GameScreen
+import com.example.kakodash.ui.screens.ItemsScreen
 import com.example.kakodash.ui.theme.KakoDashTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +22,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KakoDashTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavHost()
                 }
             }
         }
@@ -31,17 +34,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppNavHost() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KakoDashTheme {
-        Greeting("Android")
+    NavHost(
+        navController = navController,
+        startDestination = "game"
+    ) {
+        composable("game") {
+            GameScreen(navController = navController)
+        }
+        composable("items") {
+            ItemsScreen()
+        }
     }
 }
