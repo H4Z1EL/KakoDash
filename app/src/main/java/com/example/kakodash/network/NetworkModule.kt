3091aee425408f0e6c05ab25cf6b1568bf5428db
Apo1231 <20243ds166@utez.edu.mx>
2025-12-10 23:28:29 -0600

    añadi el log en el netModule

--- app/src/main/java/com/example/kakodash/network/NetworkModule.kt
+++ app/src/main/java/com/example/kakodash/network/NetworkModule.kt
@@ -1,13 +1,27 @@
 package com.example.kakodash.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkModule {
-    private const val BASE_URL = "https://api.com/api/"
+
+    private const val BASE_URL = "https://tu-api-aqui.com/api/"
 
     fun provideApiService(): ApiService {
+
+        val logger = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
