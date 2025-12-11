d29efcc34ee79b63682fc09e5eafa9d03cccd189
Mitzi Yessenia Arrieta Ignacio <20243ds176@utez.edu.mx>
2025-12-11 00:24:20 -0600

    crear estructura inicial de GameViewModel

--- app/src/main/java/com/example/kakodash/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/example/kakodash/viewmodel/GameViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.kakodash.viewmodel
 
-class GameViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+
+    private val _isGameOver = MutableStateFlow(false)
+    val isGameOver = _isGameOver.asStateFlow()
+
+    private val _playerY = MutableStateFlow(0f)
+    val playerY = _playerY.asStateFlow()
+
+    private val _obstacleX = MutableStateFlow(1f)
+    val obstacleX = _obstacleX.asStateFlow()
+
+    fun jump() {
+        // Implementación vendrá en próximos commits
+    }
+
+    fun resetGame() {
+        _playerY.value = 0f
+        _obstacleX.value = 1f
+        _isGameOver.value = false
+    }
 }
